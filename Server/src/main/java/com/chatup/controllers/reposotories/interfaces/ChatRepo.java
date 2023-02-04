7fc6a66f806aa3eb8b879af254dd36ce6d0fced8
Mahmoud Geneidi <mahmoudgeneidi98@gmail.com>
2023-02-04 23:04:15 +0200

    Adding Chat&ChatMessage (Models&Repos)

---
+++ Server/src/main/java/com/chatup/controllers/reposotories/interfaces/ChatRepo.java
@@ -0,0 +1,28 @@
+package com.chatup.controllers.reposotories.interfaces;
+
+
+import com.chatup.models.entities.Chat;
+import com.chatup.models.entities.ChatMessage;
+import com.chatup.models.entities.User;
+
+import java.util.List;
+
+public interface ChatRepo {
+
+
+    int createSingleChat(Chat singleChat);
+
+    // Read
+    Chat getSingleChat(int singleChatId);
+
+    List<User> getSingleChatUsers(int singleChatId);
+
+    List<ChatMessage> getSingleChatMessages(int singleChatId);
+
+    // Update
+    void updateSingleChat(Chat singleChat);
+
+    // Delete
+    void deleteSingleChat(int singleChatId);
+
+}

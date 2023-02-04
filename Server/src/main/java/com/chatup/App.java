7042a37e0524eed8f7587f142f630f10a4b2676b
Mahmoud Geneidi <mahmoudgeneidi98@gmail.com>
2023-02-04 23:08:33 +0200

    edit in server/app.java

--- Server/src/main/java/com/chatup/App.java
+++ Server/src/main/java/com/chatup/App.java
@@ -1,10 +1,6 @@
 package com.chatup;
 
-import com.chatup.controllers.reposotories.implementations.ChatRepoImpl;
-import com.chatup.utils.DBConnection;
-
 public class App {
     public static void main( String[] args ) {
-
     }
 }

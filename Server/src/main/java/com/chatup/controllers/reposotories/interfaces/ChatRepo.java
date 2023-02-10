93c9a4b3e78eda297295def40d2b9ef36fb61dd8
Mahmoud Geneidi <mahmoudgeneidi98@gmail.com>
2023-02-10 02:15:45 +0200

    edit in chatRepo&chatMessageRepo

--- Server/src/main/java/com/chatup/controllers/reposotories/interfaces/ChatRepo.java
+++ Server/src/main/java/com/chatup/controllers/reposotories/interfaces/ChatRepo.java
@@ -20,9 +20,9 @@ public interface ChatRepo {
     List<ChatMessage> getSingleChatMessages(int singleChatId);
 
     // Update
-    void updateSingleChat(Chat singleChat);
+    boolean updateSingleChat(Chat singleChat);
 
     // Delete
-    void deleteSingleChat(int singleChatId);
+    boolean deleteSingleChat(int singleChatId);
 
 }

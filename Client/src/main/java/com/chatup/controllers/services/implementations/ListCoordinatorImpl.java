7ffbcaeea2d711586d0befc2e5c3cf41088e64c0
Mahmoud Geneidi <mahmoudgeneidi98@gmail.com>
2023-02-11 22:06:26 +0200

    single&group(ChatMessagesServices)

--- Client/src/main/java/com/chatup/controllers/services/implementations/ListCoordinatorImpl.java
+++ Client/src/main/java/com/chatup/controllers/services/implementations/ListCoordinatorImpl.java
@@ -4,6 +4,9 @@ import com.chatup.controllers.services.interfaces.ListCoordinator;
 import com.chatup.models.entities.Card;
 import com.chatup.models.enums.UserStatus;
 import javafx.collections.ObservableList;
+import javafx.scene.layout.VBox;
+
+import java.util.HashMap;
 
 public class ListCoordinatorImpl implements ListCoordinator {
 
@@ -12,6 +15,11 @@ public class ListCoordinatorImpl implements ListCoordinator {
     private static ObservableList<Card> userGroups;
     private static ObservableList<Card> userOnlineFriends;
     private static ObservableList<Card> userOfflineFriends;
+
+    private static HashMap<Integer, VBox> singleChatMap = new HashMap<>();
+
+    private static HashMap<Integer, VBox> groupChatMap = new HashMap<>();
+
 
     private ListCoordinatorImpl(){}
 
@@ -47,5 +55,27 @@ public class ListCoordinatorImpl implements ListCoordinator {
         if(userOfflineFriends==null)
             userOfflineFriends = UserListsImpl.getUserLists().getUserFriends(UserStatus.OFFLINE);
         return userOfflineFriends;
+    }
+    @Override
+    public VBox getSingleChatVbox(int chatId){
+        if(singleChatMap.containsKey(chatId)){
+            return singleChatMap.get(chatId);
+        }
+        else{
+            VBox box = ChatServicesImpl.getChatService().getSingleChatVbox(chatId);
+            singleChatMap.put(chatId,box);
+            return box;
+        }
+    }
+    @Override
+    public VBox getGroupChatVbox(int chatId){
+        if(groupChatMap.containsKey(chatId)){
+            return groupChatMap.get(chatId);
+        }
+        else{
+            VBox box = ChatServicesImpl.getChatService().getGroupChatVbox(chatId);
+            groupChatMap.put(chatId,box);
+            return box;
+        }
     }
 }
---
+++ Client/src/main/java/com/chatup/utils/SingleChatMessage.java
@@ -0,0 +1,56 @@
+package com.chatup.utils;
+
+import com.chatup.controllers.FXMLcontrollers.ChatScreenController;
+import com.chatup.controllers.FXMLcontrollers.recievedMessageController;
+import com.chatup.controllers.FXMLcontrollers.sentMessageController;
+import com.chatup.controllers.services.implementations.UserServicesImpl;
+import com.chatup.models.entities.ChatMessage;
+import com.chatup.models.entities.User;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+
+import java.io.IOException;
+import java.util.List;
+
+public class SingleChatMessage {
+
+    public static HBox sendMessages = new HBox();
+    public static TextFlow sendMessagesContent ;
+
+    public static VBox recieveMessagesBox = new VBox();
+    public static HBox recieveMessages = new HBox();
+    public static TextFlow recieveMessageName;
+    public static TextFlow recieveMessageContent;
+
+
+    public static VBox singleMessages(List<ChatMessage> messagesList)  {
+        FXMLLoader loader;
+        VBox messages = new VBox();
+        for(ChatMessage message: messagesList ){
+            if(message.getSenderId()==1){
+                loader = new FXMLLoader(SingleChatMessage.class.getResource("/views/sentMessage.fxml"));
+                sentMessageController sentController = new sentMessageController(message.getContent());
+                loader.setController(sentController);
+            }
+            else{
+                loader = new FXMLLoader(SingleChatMessage.class.getResource("/views/recievedMessage.fxml"));
+                User user = UserServicesImpl.getUserServices().getUser(message.getSenderId());
+                recievedMessageController recievedController = new recievedMessageController(user.getUserName(),message.getContent());
+                loader.setController(recievedController);
+            }
+            try {
+                messages.getChildren().add(loader.load());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+
+        messages.setStyle("-fx-background-color: transparent;-fx-spacing: 25;");
+        return messages;
+    }
+}

aa649b07844ed1501da10802c30003d62a984b25
Karim Taha <karimtaha881@gmail.com>
2023-02-15 05:15:15 +0200

    notification, online, rightside

--- Client/src/main/java/com/chatup/controllers/FXMLcontrollers/ChatScreenController.java
+++ Client/src/main/java/com/chatup/controllers/FXMLcontrollers/ChatScreenController.java
@@ -18,6 +18,7 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
